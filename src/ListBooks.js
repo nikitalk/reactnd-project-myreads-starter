24b5f70aa40d52ad238f6b6fb4ad3ea81b8b93a6
nikitalk <nikita.lk@gmail.com>
2018-07-06 17:29:45 +0300

    feat: adding getAll from BooksAPI

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -10,17 +10,62 @@ class ListBooks extends Component {
 		console.log(books)
 
 		return (
-
-			          <div className="list-books">
+          <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-
-
-
               <div>
-                <div className="bookshelf">
+				<div className="bookshelf">
+                  	<h2 className="bookshelf-title">Currently Reading</h2>
+                  	<div className="bookshelf-books">
+					<ol className="books-grid">
+              			{books.filter((book) => book.shelf == "currentlyReading").map((book) => (
+				  	
+              			<li key={book.id}>
+
+
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors[0]}</div>
+                        </div>
+
+
+
+              			</li>
+		              	))}
+                	</ol>
+                	</div>
+              		</div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to='/search'>Add a book</Link>
+            </div>
+          </div>
+
+			)
+
+	}
+
+}
+
+export default ListBooks
+
+
+/*<div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
@@ -166,18 +211,4 @@ class ListBooks extends Component {
                       </li>
                     </ol>
                   </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to='/search'>Add a book</Link>
-            </div>
-          </div>
-
-			)
-
-	}
-
-}
-
-export default ListBooks
+                </div>*/
