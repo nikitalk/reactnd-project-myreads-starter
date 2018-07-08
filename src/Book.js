d0c15debb03f46b0e98dc033499940bd58f278ee
nikitalk <nikita.lk@gmail.com>
2018-07-08 09:54:30 +0300

    feat: adding Book component

---
+++ src/Book.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Book extends Component {
+
+
+
+render() {
+
+	const { book, onUpdating } = this.props
+
+	return (
+
+        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select value={book.shelf} onChange={(e) => onUpdating(e.target.value, book)}>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors[0]}</div>
+                        </div>
+
+		)
+}
+
+}
+
+export default Book
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class ListBooks extends Component {
 
@@ -41,22 +42,10 @@ class ListBooks extends Component {
               			<li key={book.id}>
 
 
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select value={book.shelf} onChange={(e) => onUpdateBook(e.target.value, book)}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors[0]}</div>
-                        </div>
+                      <Book 
+                        book={book}
+                        onUpdating={onUpdateBook}
+                        />
 
 
 
